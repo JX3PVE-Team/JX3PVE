a24aeedc11b52d3fd8a9abe91b492326ac2c4a1f
17229398@qq.com <17229398@qq.com>
2015-01-12 20:01:44 +0800

    宏库

--- cdn/js/main.js
+++ cdn/js/main.js
@@ -3,49 +3,61 @@ jQuery.noConflict();
 
 jQuery(function($){
 
-	//导航
-	$('#nav-folder').click(function(event) {
-		$(this).toggleClass('gray');
-		$('#menu').slideToggle().toggleClass('menufolder');;
-	});
 
-	//菜单
-	$('#menu-list li').hover(
-	function() {
-		$(this).children('a').addClass('cur').children('span').addClass('cur');
-	}, function() {
-		$(this).children('a').removeClass('cur').children('span').removeClass('cur');
-	});
+	//Header
+	//------------------------------------------
+		//导航
+		$('#nav-folder').click(function(event) {
+			$(this).toggleClass('gray');
+			$('#menu').slideToggle().toggleClass('menufolder');;
+		});
 
-	//二级菜单
-	$('#menu-list').children('li').hover(function() {
-		$(this).children('ul').slideDown();
-	}, function() {
-		$(this).children('ul').slideUp();
-	});
+		//菜单
+		$('#menu-list li').hover(
+		function() {
+			$(this).children('a').addClass('cur').children('span').addClass('cur');
+		}, function() {
+			$(this).children('a').removeClass('cur').children('span').removeClass('cur');
+		});
 
-	//白皮书作者换行
-	$(".bps-content .module li a").each(function(){
-		var book_name = $(this).html();
-		var book_arr = book_name.split('|');
-		var book_newname = book_arr.join('<br />');
-		$(this).html(book_newname);
-	})
+		//二级菜单
+		$('#menu-list').children('li').hover(function() {
+			$(this).children('ul').slideDown();
+		}, function() {
+			$(this).children('ul').slideUp();
+		});
 
-	//白皮书折叠展开
-	$('.bps-content span .toggle').on('click',function(e){
-		e.preventDefault();
-		$(this).parent('span').siblings('h4,div').slideToggle();
-	})
+	//BBS-VIEW
+	//------------------------------------------
+		//第一篇文章的padding-top，预留快速回复
+		$('#postlist').children('div').eq(0).find('.t_fsz').find('.t_f').css('padding-top','80px');
+		//加载文档后提示
+		$('#vfastpostform').find('.fullvfastpost').addClass('tipsforfastpost');
 
-	//宏库
-	var url = window.location.href;
-	var macro_url = url.search('fid=294');
-	var macro_cat = url.search('k_type=1');
-	if(macro_url!=-1 && macro_cat!=-1){
-		$('.star_hd h1 a:first').html('云端宏');
-	}
-	$('.macro-list tr:odd').addClass('even');
+
+	//BBS-LIST
+	//------------------------------------------
+		//列表间色
+		$('.macro-list tr:odd').addClass('even');
+
+
+	//ZT
+	//------------------------------------------
+		//白皮书作者换行
+		$(".bps-content .module li a").each(function(){
+			var book_name = $(this).html();
+			var book_arr = book_name.split('|');
+			var book_newname = book_arr.join('<br />');
+			$(this).html(book_newname);
+		})
+
+		//白皮书折叠展开
+		$('.bps-content span .toggle').on('click',function(e){
+			e.preventDefault();
+			$(this).parent('span').siblings('h4,div').slideToggle();
+		})
+
+	
 
 
 
